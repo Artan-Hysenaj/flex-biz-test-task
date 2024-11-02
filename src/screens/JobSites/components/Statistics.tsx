67699230a861590546e06073a060fd03df13bf90
Artan Hysenaj <ahysenaj@i-aps.com>
2024-11-02 20:07:18 +0100

    fix: statistics bg color

--- src/screens/JobSites/components/Statistics.tsx
+++ src/screens/JobSites/components/Statistics.tsx
@@ -9,28 +9,29 @@ import { JobSite } from '@/types';
 
 const Statistics = ({ data }: { data: JobSite[] | undefined }) => {
 	const statistics = useMemo(() => {
-		return data?.reduce(
-			(acc, curr) => {
+		const initial = { 'In Progress': 0, Completed: 0, 'On Hold': 0 };
+		return (
+			data?.reduce((acc, curr) => {
 				acc[curr.status] += 1;
 				return acc;
-			},
-			{ 'In Progress': 0, Completed: 0, 'On Hold': 0 }
+			}, initial) ?? initial
 		);
 	}, [data]);
 	return (
 		<Wrapper className="mb-4">
 			<div className="flex justify-between gap-2.5 p-2">
-				{statistics &&
-					Object.keys(statistics)?.map((statKey) => (
+				{Object.keys(statistics)?.map((statKey) => {
+					return (
 						<div
 							key={statKey}
+							style={{ backgroundColor: JobStatusColorsEnum[statKey] }}
 							className={cn(
-								'w-full p-8 rounded-[10px] text-center text-white font-semibold text-3xl leading-10',
-								`bg-[${JobStatusColorsEnum[statKey]}]`
+								'w-full p-8 rounded-[10px] text-center text-white font-semibold text-3xl leading-10'
 							)}>
-							{statistics[statKey as keyof typeof statistics]} {statKey}
+							{statistics?.[statKey as keyof typeof statistics]} {statKey}
 						</div>
-					))}
+					);
+				})}
 			</div>
 		</Wrapper>
 	);
